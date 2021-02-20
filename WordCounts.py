d5af79d647709583de75085badc364b5f5c86e27
midas87 <33111000+midas87@users.noreply.github.com>
2021-02-19 21:38:00 -0500

    Add files via upload

---
+++ WordCounts.py
@@ -0,0 +1,6 @@
+# World count Exercise
+
+print('Hi, What are you thinking off today?')
+words = input()
+worldCount = len(words.split())
+print("Oh nice, you just told me what's on your mind in", worldCount, 'words!')
